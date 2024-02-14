9c5f78dd9285bb6ecf50cac4c0cbfa1da062a3a0
Mathieu Sibret <mathieu.sibret@student.ucll.be>
2024-02-14 10:32:05 +0100

    startup project

---
+++ src/main/java/be/ucll/se/groep02backend/Groep02BackendApplication.java
@@ -0,0 +1,13 @@
+package be.ucll.se.groep02backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Groep02BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Groep02BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/ucll/se/groep02backend/Groep02BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ucll.se.groep02backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Groep02BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
