e69e9184763a9b4160015150368307c0fa1d7c1f
StijnAlexanderUCLL <r0787012@ucll.be>
2024-02-24 16:07:00 +0100

    deleteCar implemented

--- src/main/java/be/ucll/se/groep02backend/car/service/CarService.java
+++ src/main/java/be/ucll/se/groep02backend/car/service/CarService.java
@@ -1,17 +1,24 @@
 package be.ucll.se.groep02backend.car.service;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import be.ucll.se.groep02backend.car.model.domain.Car;
 import be.ucll.se.groep02backend.car.repo.CarRepository;
+import be.ucll.se.groep02backend.rental.model.domain.Rental;
+import be.ucll.se.groep02backend.rental.repo.RentalRepository;
 
 @Service
 public class CarService {
     @Autowired
     private CarRepository carRepository;
+
+    @Autowired
+    private RentalRepository rentalRepository;
 
     public CarService() {
     }
@@ -26,5 +33,19 @@ public class CarService {
 
     public Car findCarByRentalId(Long rentalId) {
         return carRepository.findCarByRentalsId(rentalId);
+    }
+
+    public Car deleteCar(Long id) throws CarServiceException{
+        Car car = carRepository.findCarById(id);
+        if(Objects.isNull(car)){
+            throw new CarServiceException("id", "Book with given id does not exist.");
+        }
+        Set<Rental> rentals = car.getRentals();
+        for(Rental rental : rentals){
+            car.removeRental(rental);
+            rentalRepository.delete(rental);
+        }
+        carRepository.delete(car);
+        return car;
     }
 }
