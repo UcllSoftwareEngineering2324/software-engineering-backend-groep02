7a7af0dd65e7880c642a01802787e62a242e4f5d
BenBranders0 <benbranders2@gmail.com>
2024-02-29 23:33:57 +0100

    change api

--- src/main/java/be/ucll/se/groep02backend/rent/controller/RentRestController.java
+++ src/main/java/be/ucll/se/groep02backend/rent/controller/RentRestController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,8 +40,8 @@ public class RentRestController {
         return rentService.getAllRents();
     }
     
-    @GetMapping("/get/{email}")
-    public List<Rent> getMethodName(@PathVariable String email) throws RentServiceException {
+    @GetMapping("/get/")
+    public List<Rent> getMethodName(@RequestParam String email) throws RentServiceException {
         return rentService.getRentsByEmail(email);
     }
     
