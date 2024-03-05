4f5bb861d56862743ee11edf8c4ecdfd819bc7a0
BenBranders0 <benbranders2@gmail.com>
2024-03-05 11:28:24 +0100

    added test

--- src/test/java/be/ucll/se/groep02backend/Groep02BackendApplicationTests.java
+++ src/test/java/be/ucll/se/groep02backend/Groep02BackendApplicationTests.java
@@ -1,13 +1,37 @@
 package be.ucll.se.groep02backend;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
 
 @SpringBootTest
 class Groep02BackendApplicationTests {
 
+	private static ValidatorFactory validatorFactory;
+    private static Validator validator;
+
+    @BeforeAll
+    public static void createValidator() {
+        validatorFactory = Validation.buildDefaultValidatorFactory();
+        validator = validatorFactory.getValidator();
+    }
+
+    @AfterAll
+    public static void close() {
+        validatorFactory.close();
+    }
+
 	@Test
-	void contextLoads() {
+	public void givenFalse() {
+		boolean test = false;
+		assertEquals(true, test);
 	}
 
 }
