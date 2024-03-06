ce7a5a142ce6527608eb0fd5f3b558bbe1386e4d
MathieuSibret-UCLL <113980269+MathieuSibret-UCLL@users.noreply.github.com>
2024-03-06 19:56:41 +0100

    inset querries for rental and car

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+
+INSERT INTO "PUBLIC"."CAR" VALUES
+(TRUE, 2, 5, FALSE, 1, 'Toyota', '1-ABC-123', 'Corolla', 'Sedan'),
+(TRUE, 1, 6, TRUE, 2, 'Ford', '1-DEF-456', 'F-150', 'Pickup'),
+(FALSE, 2, 5, FALSE, 3, 'Honda', '1-GHI-789', 'Civic', 'Sedan'),
+(TRUE, 2, 8, TRUE, 4, 'Chevrolet', '1-JKL-012', 'Suburban', 'SUV'),
+(FALSE, 2, 5, FALSE, 5, 'Tesla', '1-MNO-345', 'Model 3', 'Sedan');              
+
+
+
+INSERT INTO "PUBLIC"."RENTAL" VALUES
+(DATE '2024-04-07', 0, DATE '2024-04-01', 0, 1, 1, 'New York', 'nyrental@example.com', '123-456-7890', NULL),
+(DATE '2024-04-26', 0, DATE '2024-04-15', 0, 2, 2, 'Los Angeles', 'larental@example.com', '987-654-3210', NULL),
+(DATE '2024-05-31', 0, DATE '2024-04-24', 0, 3, 3, 'Chicago', 'chirental@example.com', '555-123-4567', NULL);          
