3a8231fa8479ba49d83897db4942c053381e3767
ward-ucll <113981513+ward-ucll@users.noreply.github.com>
2024-03-12 20:26:12 +0100

    notification backend

--- src/main/java/be/ucll/se/groep02backend/notification/model/Notification.java
+++ src/main/java/be/ucll/se/groep02backend/notification/model/Notification.java
@@ -3,19 +3,78 @@ package be.ucll.se.groep02backend.notification.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Table;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
-
 @Entity
-@Table(name = "notification")   
+@Table(name = "notification")
 public class Notification {
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Id
     public long id;
-    
-    @NotBlank(message="Title is required")
+
+    @NotBlank(message = "Title is required")
     private String title;
+
+    @NotBlank(message = "content is required")
+    private String content;
+
+    @Valid
+    private Boolean isActive;
+
+    @Valid
+    private ActionType action;
+
+    public Notification() {
+    }
+
+    public Notification(String title, String content, Boolean isActive, ActionType action) {
+        setTitle(title);
+        setContent(content);
+        setActive(isActive);
+        setAction(action);
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+
+    public Boolean getActive() {
+        return this.isActive;
+    }
+
+    public ActionType getAction() {
+        return this.action;
+    }
+
+    public void setTitle(String newtitle) {
+        this.title = newtitle;
+    }
+
+    public void setContent(String newContent) {
+        this.content = newContent;
+    }
+
+    public void setActive(Boolean newIsActive) {
+        this.isActive = newIsActive;
+    }
+
+    public void setAction(ActionType newAction) {
+        this.action = newAction;
+    }
+
+
+    public enum ActionType {
+        None, Confirm_rent;
+    }
+
+
+    
 
 }
--- src/main/java/be/ucll/se/groep02backend/notification/service/NotificationService.java
+++ src/main/java/be/ucll/se/groep02backend/notification/service/NotificationService.java
@@ -1,5 +1,50 @@
 package be.ucll.se.groep02backend.notification.service;
 
+import java.util.List;
+import java.util.Objects;
+
+import be.ucll.se.groep02backend.notification.model.Notification;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import be.ucll.se.groep02backend.notification.repo.NotificationRepository;
+
+@Service
 public class NotificationService {
-    
+    @Autowired
+    private NotificationRepository notificationRepository;
+
+    public NotificationService() {}
+
+    public List<Notification> findAll() {
+        return notificationRepository.findAll();
+    }
+
+    public Notification addNotification(Notification notification) {
+        return notificationRepository.save(notification);
+    }
+
+    public Notification findNotificationById(Long id) {
+        return notificationRepository.findNotificationById(id);
+    }
+
+    public Notification deleteNotification(Long id) throws NotificationServiceException{
+        Notification notification = notificationRepository.findNotificationById(id);
+        if(Objects.isNull(notification)){
+            throw new NotificationServiceException("id", "Notification with given id does not exist.");
+        }
+        notificationRepository.delete(notification);
+        return notification;
+    }
+
+    public Notification completeNotification(Long id) throws NotificationServiceException {
+        Notification notification = notificationRepository.findNotificationById(id);
+        if(Objects.isNull(notification)){
+            throw new NotificationServiceException("id", "Notification with given id does not exist.");
+        }
+        notification.setActive(false);
+        return notificationRepository.save(notification);
+    }
+
 }
