48158b989c3b56972c8fd826b6b2972ad0325eff
Mathieu Sibret <mathieu.sibret@student.ucll.be>
2024-03-14 00:20:33 +0100

    removed unused lines

--- src/main/java/be/ucll/se/groep02backend/notification/controller/NotificationRestController.java
+++ src/main/java/be/ucll/se/groep02backend/notification/controller/NotificationRestController.java
@@ -8,12 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +18,6 @@ import org.springframework.web.bind.annotation.RestController;
 import be.ucll.se.groep02backend.notification.model.Notification;
 import be.ucll.se.groep02backend.notification.service.NotificationService;
 import be.ucll.se.groep02backend.notification.service.NotificationServiceException;
-import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
--- src/main/java/be/ucll/se/groep02backend/rent/model/domain/Rent.java
+++ src/main/java/be/ucll/se/groep02backend/rent/model/domain/Rent.java
@@ -17,7 +17,6 @@ import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
-import jakarta.validation.Valid;
 // import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Future;
