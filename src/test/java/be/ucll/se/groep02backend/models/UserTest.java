770118c1d797633e833deda5eaa76a19973c3913
BenBranders0 <benbranders2@gmail.com>
2024-04-23 17:41:19 +0200

    added model test files

---
+++ src/test/java/be/ucll/se/groep02backend/models/UserTest.java
@@ -0,0 +1,34 @@
+package be.ucll.se.groep02backend.models;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+
+public class UserTest {
+    
+    // validator
+    private static ValidatorFactory validatorFactory;
+    private static Validator validator;
+
+
+    @BeforeAll
+    public static void createValidator() {
+        validatorFactory = Validation.buildDefaultValidatorFactory();
+        validator = validatorFactory.getValidator();
+    }
+
+    @AfterAll
+    public static void close() {
+        validatorFactory.close();
+    }
+
+    @BeforeEach
+    public void setUp() {}
+    
+}
