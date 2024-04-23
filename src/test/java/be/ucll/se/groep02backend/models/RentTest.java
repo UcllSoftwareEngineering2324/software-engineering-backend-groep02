f2031e26120ca4702fcc490c28f468809aea298f
BenBranders0 <benbranders2@gmail.com>
2024-04-23 22:58:50 +0200

    added rent test model

--- src/test/java/be/ucll/se/groep02backend/models/RentTest.java
+++ src/test/java/be/ucll/se/groep02backend/models/RentTest.java
@@ -1,10 +1,18 @@
 package be.ucll.se.groep02backend.models;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.LocalDate;
+import java.util.Set;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import be.ucll.se.groep02backend.rent.model.domain.Rent;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validation;
 import jakarta.validation.Validator;
@@ -12,10 +20,14 @@ import jakarta.validation.ValidatorFactory;
 
 public class RentTest {
 
+    private LocalDate validStartDate = LocalDate.parse("2024-05-23");
+    private LocalDate validEndDate = LocalDate.parse("2024-07-23");
+
+    private Rent rentOne;
+
     // validator
     private static ValidatorFactory validatorFactory;
     private static Validator validator;
-
 
     @BeforeAll
     public static void createValidator() {
@@ -29,6 +41,80 @@ public class RentTest {
     }
 
     @BeforeEach
-    public void setUp() {}
+    public void setUp() {
+        rentOne = new Rent(validStartDate, validEndDate);
+    }
+
+    @Test
+    public void givenValidStartDateEndDate_whenCreatingRent_thenRentIsCreatedWithThatStartDateEndDate() {
+        assertNotNull(rentOne);
+        assertEquals(validStartDate, rentOne.getStartDate());
+        assertEquals(validEndDate, rentOne.getEndDate());
+        Set<ConstraintViolation<Rent>> violations = validator.validate(rentOne);
+        assertTrue(violations.isEmpty());
+    }
+
+    @Test
+    public void givenInvalidStartDate_whenRentIsCreated_thenStartDateViolationMessageIsThrown() {
+        // when
+        LocalDate inValidStartDate = null;
+        Rent rent = new Rent(inValidStartDate, validEndDate);
+
+        // then
+        Set<ConstraintViolation<Rent>> violations = validator.validate(rent);
+        assertEquals(violations.size(), 1);
+        ConstraintViolation<Rent> violation = violations.iterator().next();
+        assertEquals("Start date is required", violation.getMessage());
+        assertEquals("startDate", violation.getPropertyPath().toString());
+        assertEquals(inValidStartDate, violation.getInvalidValue());
+
+    }
+
+    @Test
+    public void givenStartDateNotInFuture_whenRentIsCreated_thenStartDateViolationMessageIsThrown() {
+        // when
+        LocalDate inValidStartDate = LocalDate.parse("2023-05-23");
+        Rent rent = new Rent(inValidStartDate, validEndDate);
+
+        // then
+        Set<ConstraintViolation<Rent>> violations = validator.validate(rent);
+        assertEquals(violations.size(), 1);
+        ConstraintViolation<Rent> violation = violations.iterator().next();
+        assertEquals("Start date is invalid, it has to be in the future", violation.getMessage());
+        assertEquals("startDate", violation.getPropertyPath().toString());
+        assertEquals(inValidStartDate, violation.getInvalidValue());
+
+    }
+
+    @Test
+    public void givenInvalidEndDate_whenRentIsCreated_thenEndDateViolationMessageIsThrown() {
+        // when
+        LocalDate inValidEndDate = null;
+        Rent rent = new Rent(validStartDate, inValidEndDate);
+
+        // then
+        Set<ConstraintViolation<Rent>> violations = validator.validate(rent);
+        assertEquals(violations.size(), 1);
+        ConstraintViolation<Rent> violation = violations.iterator().next();
+        assertEquals("End date is required", violation.getMessage());
+        assertEquals("endDate", violation.getPropertyPath().toString());
+        assertEquals(inValidEndDate, violation.getInvalidValue());
+
+    }
+
+    @Test
+    public void givenEndDateNotInFuture_whenRentIsCreated_thenEndDateViolationMessageIsThrown() {
+        // when
+        LocalDate inValidEndDate = LocalDate.parse("2023-05-23");
+        Rent rent = new Rent(validStartDate, inValidEndDate);
+
+        // then
+        Set<ConstraintViolation<Rent>> violations = validator.validate(rent);
+        assertEquals(violations.size(), 1);
+        ConstraintViolation<Rent> violation = violations.iterator().next();
+        assertEquals("End date is invalid, it has to be in the future", violation.getMessage());
+        assertEquals("endDate", violation.getPropertyPath().toString());
+        assertEquals(inValidEndDate, violation.getInvalidValue());
+    }
     
 }
