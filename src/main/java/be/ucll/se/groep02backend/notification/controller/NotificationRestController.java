83a8b36fc900bc4c8e6d2e2c15ce108242fb5bf6
ward-ucll <113981513+ward-ucll@users.noreply.github.com>
2024-05-07 12:24:14 +0200

    This absolutely not janky

--- src/main/java/be/ucll/se/groep02backend/notification/controller/NotificationRestController.java
+++ src/main/java/be/ucll/se/groep02backend/notification/controller/NotificationRestController.java
@@ -1,14 +1,19 @@
 package be.ucll.se.groep02backend.notification.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import be.ucll.se.groep02backend.config.ApplicationConfig;
